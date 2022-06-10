3f3dccdd3e6f5f0cb70e85a66f41237964df12f9
Shamim Reza <wpshamim@gmail.com>
2022-06-11 02:12:47 +0600

    filter lunch items to foods

---
+++ src/Components/Home/Foods/Lunch/Lunch.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Lunch = ({ lunch }) => {
+    const { picture, title, description, price } = lunch;
+    return (
+        <div>
+            <h1>Lunch</h1>
+        </div>
+    );
+};
+
+export default Lunch;
