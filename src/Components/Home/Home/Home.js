f196193e43de37b69ce80363036d20931d1e3149
Shamim Reza <wpshamim@gmail.com>
2022-06-11 02:13:04 +0600

    home section added

---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Foods from "../Foods/Foods/Foods";
+
+const Home = () => {
+    return (
+        <main>
+            <Foods></Foods>
+        </main>
+    );
+};
+
+export default Home;
