88b3dfacd6a12e3cc89c380bbcef6002d96c5ce7
Shamim Reza <wpshamim@gmail.com>
2022-06-11 17:46:29 +0600

    added header menu

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../Assets/logo2.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
-const Header = () => {
+const Header = ({ cart }) => {
     return (
-        <div>
-            <h1>this is header</h1>
+        <div className="flex justify-between container mx-auto items-center py-5">
+            <Link to="/">
+                <img className="w-2/6" src={logo} alt="" />
+            </Link>
+            <div className="menu">
+                <ul className="flex gap-10">
+                    <li className="block mt-4 lg:inline-block lg:mt-0 lg:ml-6 align-middle text-black hover:text-gray-700">
+                        <Link className="relative flex" to="/cart">
+                            <FontAwesomeIcon className="flex-1 w-8 h-8" icon={faCartShopping} />
+                            <span className="absolute right-0 top-0 rounded-full bg-red-600 w-4 h-4 top right p-0 m-0 text-white font-mono text-sm  leading-tight text-center">{cart.length}</span>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/signup">Sign up</Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
