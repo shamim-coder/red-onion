4e18e2c29b71ef0b5174f9b38bf1a4792378ddfe
Shamim Reza <wpshamim@gmail.com>
2022-06-11 17:48:35 +0600

    implement active link on menu

--- src/Components/Home/Foods/Foods/Foods.js
+++ src/Components/Home/Foods/Foods/Foods.js
@@ -1,27 +1,48 @@
-import React from "react";
+import React, { useContext } from "react";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import { CartContext } from "../../../../App";
 import useFoods from "../../../../Hooks/useFoods";
-import Breakfast from "../Breakfast/Breakfast";
-import Dinner from "../Dinner/Dinner";
-import Lunch from "../Lunch/Lunch";
 
 const Foods = () => {
-    const [foods] = useFoods();
+    const activeStyle = {
+        textDecoration: "underline",
+        color: "orange",
+    };
 
+    const navigate = useNavigate();
+
+    const [cart] = useContext(CartContext);
+
+    const [foods] = useFoods("data.json");
     const breakfastFood = foods.filter((food) => food.category === "breakfast");
     const lunchFood = foods.filter((food) => food.category === "lunch");
     const dinnerFood = foods.filter((food) => food.category === "dinner");
 
     return (
-        <section>
-            {breakfastFood.map((breakfast) => (
-                <Breakfast key={breakfast.id} breakfast={breakfast} />
-            ))}
-            {lunchFood.map((lunch) => (
-                <Lunch key={lunch.id} lunch={lunch} />
-            ))}
-            {dinnerFood.map((dinner) => (
-                <Dinner key={dinner.id} dinner={dinner} />
-            ))}
+        <section className="container mx-auto ">
+            <ul className="flex gap-10 justify-center py-20">
+                <li>
+                    <NavLink style={({ isActive }) => (isActive ? activeStyle : undefined)} to="/breakfast">
+                        Breakfast
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink style={({ isActive }) => (isActive ? activeStyle : undefined)} to="/lunch">
+                        Lunch
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink style={({ isActive }) => (isActive ? activeStyle : undefined)} to="/dinner">
+                        Dinner
+                    </NavLink>
+                </li>
+            </ul>
+
+            <Outlet context={{ breakfastFood, lunchFood, dinnerFood }} />
+
+            <button disabled={cart.length === 0 && true} onClick={() => navigate("/cart")} className={`${cart.length === 0 ? "bg-slate-300" : "bg-[#F91944]"} text-white px-10 py-3 mx-auto block rounded-lg mt-10`}>
+                Checkout Your Foods
+            </button>
         </section>
     );
 };
